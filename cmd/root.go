e0b59e08976252b879b3d7ca04e5a3daebb96a02
Dakota Walsh <kota@nilsu.org>
2020-11-22 12:02:33 -0500

    print battery percentage and status

--- cmd/root.go
+++ cmd/root.go
@@ -19,28 +19,29 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
+	"io/ioutil"
 	"os"
+	"strconv"
+	"strings"
 
 	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
+const now string = "/sys/class/power_supply/BAT0/energy_now"
+const max string = "/sys/class/power_supply/BAT0/energy_full"
+const status string = "/sys/class/power_supply/BAT0/status"
+
 var cfgFile string
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "betterbattery",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A battery printing utility.",
+	Long:  `betterbattery prints the battery percentage, status, and can run a command if the percentage fell below a specified value since it was last ran.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		bb()
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -54,19 +55,9 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.betterbattery.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
 		// Use config file from the flag.
@@ -78,16 +69,41 @@ func initConfig() {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-
-		// Search config in home directory with name ".betterbattery" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigName(".betterbattery")
 	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
+	viper.AutomaticEnv()
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
+}
+
+func bb() {
+	n := read(now)
+	m := read(max)
+	s := read(status)
+	ni, err := strconv.Atoi(n)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "betterbattery: %v\n", err)
+	}
+	mi, err := strconv.Atoi(m)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "betterbattery: %v\n", err)
+	}
+	fmt.Println(percent(ni, mi))
+	fmt.Println(s)
+}
+
+func read(p string) string {
+	v, err := ioutil.ReadFile(p)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "betterbattery: reading %s: %v\n", p, err)
+		os.Exit(1)
+	}
+	return strings.TrimSuffix(string(v), "\n")
+}
+
+// percent returns a percentage of two ints
+func percent(n, m int) int {
+	return int(float32(n) / (float32(m) / 100))
 }
